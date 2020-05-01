87c573a0e0da7e98c0cfd9e5452cd89a9335fdf4
Simon Marecek <cj.marecek@gmail.com>
2020-05-01 19:35:08 +0200

    backend finnished

--- dbSchema.js
+++ dbSchema.js
@@ -1,4 +1,17 @@
 let db = {
+  users: [
+    {
+      userId: 'aalskdfjsladfkj',
+      email: 'user@email.com',
+      handle: 'user',
+      createdAt: '2019-03-15T10:59:52.798Z',
+      imageUrl: 'image/asljlsdkfjsldfkjslf',
+      bio: 'Hello, my name is user, nice to meet you.',
+      website: 'https://user.com',
+      location: 'London, UK',
+    },
+  ],
+
   screams: [
     {
       userHandle: 'user',
@@ -8,4 +21,46 @@ let db = {
       commentCount: 2,
     },
   ],
+  comments: [
+    {
+      userHandle: 'user',
+      screamId: 'asdklaksjfhaskldfj',
+      body: 'Nice one mate!',
+      createdAt: '2020-04-02T20:35:37.549Z',
+    }
+  ],
+  notifications: [
+    {
+      recipient: 'user', 
+      sender: 'john', 
+      read: 'true | false', 
+      screamId: 'like | comment',
+      type: 'like | comment',
+      createdAt: '2020-04-02T20:35:37.549Z'
+    }
+  ]
 };
+
+const userDetails = {
+  //Redux data
+  credentials: {
+    userId: 'aalskdfjsladfkj',
+    email: 'user@email.com',
+    handle: 'user',
+    createdAt: '2019-03-15T10:59:52.798Z',
+    imageUrl: 'image/asljlsdkfjsldfkjslf',
+    bio: 'Hello, my name is user, nice to meet you.',
+    website: 'https://user.com',
+    location: 'London, UK',
+  },
+  likes: [
+    {
+      userHandle: 'user',
+      screamId: 'asaskldjfhaskldjf'
+    },
+    {
+      userHandle: 'user',
+      screamId: 'asdfoweroskdflsdf'
+    },
+  ]
+}
--- functions/util/validators.js
+++ functions/util/validators.js
@@ -37,3 +37,19 @@ exports.validateLoginData = (data) => {
     valid: Object.keys(errors).length === 0 ? true : false,
   };
 };
+
+exports.reduceUserDetails = (data) => {
+  let userDetails = {};
+
+  //trim to remove any white space
+  if(!isEmpty(data.bio.trim())) userDetails.bio = data.bio;
+  if(!isEmpty(data.website.trim())){
+    // https://website.com
+    if(data.website.trim().substring(0,4) !== 'http'){
+      userDetails.website = `http://${data.website.trim()}`
+    } else userDetails.website = data.website;
+  }
+  if(!isEmpty(data.location.trim())) userDetails.location = data.location;
+
+  return userDetails;
+}
